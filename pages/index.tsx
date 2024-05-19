6aed5ee83134f143017d4447e63dfa25b3026dbf
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2024-05-19 14:25:12 -0500

    Se agrega vista inicial de dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,29 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <main className='h-screen w-full flex items-center justify-center'>Hola mundo</main>;
+  return (
+    <main className="h-screen w-full flex items-center justify-center">
+      <Link className="p-3 m-2 bg-blue-500" href="/dashboard">
+        Dashboard
+      </Link>
+
+      <Link className="p-3 m-2 bg-blue-500" href="/admin/books">
+        Books
+      </Link>
+
+      <Link className="p-3 m-2 bg-blue-500" href="/admin/users">
+        Users
+      </Link>
+
+      <Link className="p-3 m-2 bg-blue-500" href="/admin/users/1">
+        User Detail
+      </Link>
+
+      <Link className="p-3 m-2 bg-blue-500" href="/admin/transactions">
+        Transactions
+      </Link>
+    </main>
+  );
 };
 
 export default Home;
