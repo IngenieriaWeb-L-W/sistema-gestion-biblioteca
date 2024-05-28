5f99e532e55190101536afecc1816fe14bd8b1ea
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2024-05-28 07:08:10 -0500

    Se corrigen errores para build

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -5,8 +5,8 @@ type Data = {
   name: string;
 };
 
-export function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.status(200).json({ name: "John Doe" });
-}
+};
 
 export { handler as GET };
